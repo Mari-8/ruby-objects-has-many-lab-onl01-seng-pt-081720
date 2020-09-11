388447e49b237499d4b062a3614fb62ce40e7463
Ryan Altimari <ryanaltimari8@gmail.com>
2020-09-11 20:39:13 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,23 @@
+class Post
+  attr_accessor :author, :title
+  
+  @@all = []
+  
+  def initialize(name)
+    @title = title 
+    @@all << self
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def author_name 
+    self.author
+  end
+  
+  def title
+    return self.title
+    
+  end
+end
